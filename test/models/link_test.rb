f8b80c5f45bff038fa78e72ad964bb6c143e569e
Manuel Lorenzo <mlorenzo@mobiquityinc.com>
2015-04-02 16:56:18 +0200

    started adding unit tests

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not create link without title and url" do
+  	link = Link.new
+  	assert_not link.save, "Cannot save link without title and url"
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test  "should not save new empty user" do
+  	user = User.new
+    assert_not user.save
+  end
 end
